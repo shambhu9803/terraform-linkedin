c9e0350530a8bfd13ce9982f29add4dcd8268048
Your Name <you@example.com>
2020-06-29 21:49:46 -0400

    subnet and security

--- resource.tf
+++ resource.tf
@@ -7,3 +7,33 @@ resource "aws_vpc" "environment_example_two" {
     }
 
 }
+
+resource "aws_subnet" "subnet-1" {
+    cidr_block = cidrsubnet(aws_vpc.environment_example_two.cidr_block, 3, 1)
+    vpc_id = aws_vpc.environment_example_two.id
+    availability_zone = "us-east-2a"
+    tags = {
+        Name = "private-tf-1a"
+}
+}
+
+resource "aws_subnet" "subnet-2" {
+    cidr_block = cidrsubnet(aws_vpc.environment_example_two.cidr_block, 2, 2)
+    vpc_id = aws_vpc.environment_example_two.id
+    availability_zone = "us-east-2b"
+    tags = {
+        Name = "private-tf-2a"
+}
+}
+
+resource "aws_security_group" "subnetsecurity" {
+    vpc_id = aws_vpc.environment_example_two.id
+    ingress {
+        cidr_blocks = [
+            aws_vpc.environment_example_two.cidr_block
+        ]
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+    }
+}
